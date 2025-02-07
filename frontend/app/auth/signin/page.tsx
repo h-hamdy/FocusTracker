624e77365fe6568653af6150dbfe5ae0ad236497
h-hamdy <houssamhamdy223@gmail.com>
2025-02-07 17:37:43 +0100

    WIP: Authentication updates

--- frontend/app/auth/signin/page.tsx
+++ frontend/app/auth/signin/page.tsx
@@ -2,25 +2,21 @@
 import React from "react";
 
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useRouter } from "next/navigation";
-import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { SigninSchemaType, signinSchema } from "../dto/signinSchema";
 import { z } from "zod";
 import axios from "axios";
-
-
 
 // SignIn component handles user login for the FocusTrack app
 export default function Signin() {
@@ -29,80 +25,95 @@ export default function Signin() {
   const form = useForm<SigninSchemaType>({
     resolver: zodResolver(signinSchema),
     defaultValues: {
-	username: "",
+      username: "",
       password: "",
     },
   });
 
   const onSubmit = async (values: z.infer<typeof signinSchema>) => {
-	console.log(values)
+    console.log(values);
     try {
-		await axios.post("http://localhost:3001/auth/signin", values, {withCredentials: true});
-		router.push("/")
-	}
-	catch (error) {
-		console.log(error);
-	}
+      await axios.post("http://localhost:3001/auth/signin", values, {
+        withCredentials: true,
+      });
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="flex items-center justify-center h-screen w-full">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Sign In to FocusTrack</CardTitle>
-          <CardDescription className="text-xs pt-1">
-            Enter your email and password to access your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-		<Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col w-full gap-4">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <Label htmlFor="username">Username</Label>
-              <FormControl>
-                <Input {...field} id="username" placeholder="Enter your Username" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      <div className="w-[350px]">
+        <div className="flex flex-col gap-4">
+          <div>
+            <div className="text-black justify-center pb-8 flex items-center font-extrabold text-2xl cursor-pointer">
+              <img className="w-[40px]" src="/images.png"></img>
+              FocusTracker
+            </div>
+          </div>
+          <div>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col w-full gap-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="username">Username</Label>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          id="username"
+                          placeholder="Enter your Username"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <Label htmlFor="password">Password</Label>
-              <FormControl>
-                <Input {...field} id="password" type="password" placeholder="Enter your password" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="password">Password</Label>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          id="password"
+                          type="password"
+                          placeholder="Enter your password"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-        <Button  type="submit" className="w-full">
-          Sign in
-        </Button>
-      </form>
-    </Form>
-        </CardContent>
-        <CardFooter className="flex items-center justify-center">
-          <p className="text-sm text-muted-foreground">
-            You Don't have an account?{" "}
-            <a
-              onClick={() => router.push("/auth/signup")}
-              className="text-primary hover:underline cursor-pointer"
-            >
-              Sign Up
-            </a>
-          </p>
-        </CardFooter>
-      </Card>
+                <Button type="submit" className="w-full">
+                  Sign in
+                </Button>
+              </form>
+            </Form>
+          </div>
+          <div className="flex items-center justify-center">
+            <p className="text-sm text-muted-foreground">
+              You Don't have an account?{" "}
+              <a
+                onClick={() => router.push("/auth/signup")}
+                className="text-primary hover:underline cursor-pointer"
+              >
+                Sign Up
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/app/auth/signup/page.tsx
+++ frontend/app/auth/signup/page.tsx
@@ -2,14 +2,6 @@
 import React, { useState } from "react";
 
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { redirect } from "next/navigation";
@@ -55,98 +47,100 @@ export default function Signup() {
 
   return (
     <div className="flex items-center justify-center h-screen w-full">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Sign Up to FocusTrack</CardTitle>
-          <CardDescription className="text-xs pt-1">
-            Create your account to start tracking your productivity
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="flex flex-col w-full gap-4"
-            >
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <Label htmlFor="username">Username</Label>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        id="username"
-                        placeholder="Enter your username"
-                        className="placeholder:text-sm"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+      <div className="w-[350px]">
+        <div className="flex flex-col gap-4">
+          <div>
+            <div className="text-black justify-center pb-8 flex items-center font-extrabold text-2xl cursor-pointer">
+              <img className="w-[40px]" src="/images.png"></img>
+              FocusTracker
+            </div>
+          </div>
+          <div>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col w-full gap-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="username">Username</Label>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          id="username"
+                          placeholder="Enter your username"
+                          className="placeholder:text-sm"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <Label htmlFor="password">Password</Label>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        id="password"
-                        type="password"
-                        placeholder="Enter your password"
-                        className="placeholder:text-sm"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="password">Password</Label>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          id="password"
+                          type="password"
+                          placeholder="Enter your password"
+                          className="placeholder:text-sm"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <FormField
-                control={form.control}
-                name="confirmationPassword"
-                render={({ field }) => (
-                  <FormItem>
-                    <Label htmlFor="confirmationPassword">
-                      Confirm Password
-                    </Label>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        id="confirmationPassword"
-                        type="password"
-                        placeholder="Confirm your password"
-                        className="placeholder:text-sm"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                <FormField
+                  control={form.control}
+                  name="confirmationPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="confirmationPassword">
+                        Confirm Password
+                      </Label>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          id="confirmationPassword"
+                          type="password"
+                          placeholder="Confirm your password"
+                          className="placeholder:text-sm"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <Button type="submit" className="w-full">
-                Sign Up
-              </Button>
-            </form>
-          </Form>
-        </CardContent>
-        <CardFooter className="flex items-center justify-center">
-          <p className="text-sm text-muted-foreground">
-            Already have an account?{" "}
-            <a
-              onClick={() => redirect("/auth/signin")}
-              className="text-primary hover:underline cursor-pointer"
-            >
-              Sign In
-            </a>
-          </p>
-        </CardFooter>
-      </Card>
+                <Button type="submit" className="w-full">
+                  Sign Up
+                </Button>
+              </form>
+            </Form>
+          </div>
+          <div className="flex items-center justify-center">
+            <p className="text-sm text-muted-foreground">
+              Already have an account?{" "}
+              <a
+                onClick={() => redirect("/auth/signin")}
+                className="text-primary hover:underline cursor-pointer"
+              >
+                Sign In
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
